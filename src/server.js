e0a7a2e846b68c26afe607e8ed41c2b7711edc33
taha najjar <tahayassine2000najjar@gmail.com>
2026-03-15 23:50:03 +0000

    Initial commit: SubLedger API with Auth and Subscriptions

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const app = require('./app');
+const connectDB = async () => {
+  const mongoose = require('mongoose');
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
